7cdc0ef3e1f70254f186ca7cc6f325e1923840dd
Bertino <albertgeanta@gmail.com>
2021-09-20 10:59:54 +0000

    Adapt trees for dot

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond1lte.mk
+    $(LOCAL_DIR)/dot_beyond1lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond1lte-eng \
-    lineage_beyond1lte-user \
-    lineage_beyond1lte-userdebug
+    dot_beyond1lte-eng \
+    dot_beyond1lte-user \
+    dot_beyond1lte-userdebug
